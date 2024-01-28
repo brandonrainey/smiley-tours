89c2ee046429073ccdb22f62d521c6fd0852f775
Brandon Rainey <brandonhrainey@gmail.com>
2024-01-27 19:38:37 -0500

    changed redirects file

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: 'https://www.smileytour.netlify.app',
-                destination: 'https://www.smileytour.guide',
-                permanent: true,
-            },
-        ]
-    }
+ 
 }
 
 module.exports = nextConfig
