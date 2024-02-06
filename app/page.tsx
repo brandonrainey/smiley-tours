8f189390837687a843116ae65cbda7c119678b9b
Brandon Rainey <brandonhrainey@gmail.com>
2024-02-05 22:22:34 -0500

    dynamicly rendering some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,37 @@
 
 
-import Footer from '@/components/Footer'
+// import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import InfoSidebar from '@/components/InfoSidebar'
 import MainContent from '@/components/MainContent'
 import TourSidebar from '@/components/TourSidebar'
-import Testimonials from '@/components/Testimonials'
+// import Testimonials from '@/components/Testimonials'
 import MobileNav from '@/components/MobileNav'
-import ContentSectionOne from '@/components/ContentSectionOne'
-import ContentSectionTwo from '@/components/ContentSectionTwo'
-import ContentSectionThree from '@/components/ContentSectionThree'
+// import ContentSectionOne from '@/components/ContentSectionOne'
+// import ContentSectionTwo from '@/components/ContentSectionTwo'
+// import ContentSectionThree from '@/components/ContentSectionThree'
 import AreaSelector from '@/components/AreaSelector'
+import dynamic from 'next/dynamic'
+
+const ContentSectionOne = dynamic(() => import('@/components/ContentSectionOne'), {
+  ssr: false,
+});
+
+const ContentSectionTwo = dynamic(() => import('@/components/ContentSectionTwo'), {
+  ssr: false,
+});
+
+const ContentSectionThree = dynamic(() => import('@/components/ContentSectionThree'), {
+  ssr: false,
+});
+
+const Testimonials = dynamic(() => import('@/components/Testimonials'), {
+  ssr: false,
+});
+
+const Footer = dynamic(() => import('@/components/Footer'), {
+  ssr: false,
+});
 
 export default function Home() {
   
