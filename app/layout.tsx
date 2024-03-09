aad113e82d774692285403d35aab20657ca876e1
Brandon Rainey <brandonhrainey@gmail.com>
2024-03-08 22:56:36 -0500

    updated description meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Varela_Round, Carter_One } from 'next/font/google'
 export const metadata: Metadata = {
   title: 'Smiley Tour - Asakusa Tokyo Tour Guide',
   description:
-    'Asakusa Tokyo Tour Guide by a former Japanese Idol! Learn about the history of Asakusa and experience activities famous and unique to Japan.',
+    'Asakusa Tokyo English-speaking Tour Guide by a former Japanese Idol! Learn about the history of Asakusa, Ueno, Akihabara and experience activities famous and unique to Japan.',
 }
 
 const varelaRound = Varela_Round({
