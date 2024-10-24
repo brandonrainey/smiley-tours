96367075fe5ab7a818e69304a38a03386fce3aef
Brandon Rainey <brandonhrainey@gmail.com>
2024-10-23 23:40:18 -0400

    added new special event tour, fixed bar event spacing

--- components/EventSidebar.tsx
+++ components/EventSidebar.tsx
@@ -47,6 +47,34 @@ export default function EventSidebar() {
         </div>
 
 
+
+
+        <div className="text-center font-semibold flex flex-col items-center justify-between mt-8 sm:mt-0 sm:h-[414px]">
+          <h2
+            className={`text-center text-2xl tracking-wide font-semibold text-white border-y-2 border-pink-300 custom:text-shadow-1 text-shadow-5 py-1 mb-4`}
+          >
+            Japan's Heroic Journey Tour
+          </h2>
+
+          <a
+            className="flex flex-col items-center "
+            href="https://smileywalk.shop/items/670d24723626e42220ec3173"
+          >
+            <img
+              src="/tours/journeyTour.webp"
+              alt="private tour"
+              className="rounded max-w-[300px] custom:w-full w-[300px] tourShadow object-cover aspect-square"
+            />
+          </a>
+
+          <div
+            className={`font-semibold rounded text-white custom:text-shadow-1 text-shadow-2 mt-2  max-w-[300px]  p-1 w-full flex flex-col items-center`}
+          >
+            
+          </div>
+        </div>
+
+
         <div className="text-center font-semibold flex flex-col items-center justify-between mt-8 sm:mt-0 sm:h-[414px]">
           <h2
             className={`text-center text-2xl tracking-wide font-semibold text-white border-y-2 border-pink-300 custom:text-shadow-1 text-shadow-5 py-1 mb-4`}
@@ -75,14 +103,14 @@ export default function EventSidebar() {
         </div>
 
         {/* Miley's Bar */}
-        <div className="text-center font-semibold flex flex-col items-center mt-8 sm:mt-0 sm:h-[414px]">
+        <div className="text-center font-semibold flex flex-col items-center mt-8 sm:mt-0 sm:h-[414px] justify-between">
           <h2
             className={`text-center text-2xl tracking-wide font-semibold text-white border-y-2 border-pink-300 custom:text-shadow-1 text-shadow-5 py-1 mb-4`}
           >
             Miley&apos;s Bar
           </h2>
 
-          <div className="flex flex-col items-center mt-auto">
+          <div className="flex flex-col items-center ">
             <img
               src="/bar.webp"
               alt="ninja bar"
